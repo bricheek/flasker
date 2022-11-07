bf9aefa87c9e2c0f574db1cfb66da237caaed32a
bricheek <bricheek@gmail.com>
2022-11-07 17:25:04 -0500

    created and connected mysql db

--- hello.py
+++ hello.py
@@ -7,9 +7,12 @@ from datetime import datetime
 
 #create flask instance
 app = Flask(__name__)
+# old Sqlite db
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# new MySQL db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/my_users'
 app.config['SECRET_KEY'] = "secret key ###"
-#add database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
 db = SQLAlchemy(app)
 db.init_app(app)
 
@@ -25,8 +28,9 @@ class Users(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.name
 
-with app.app_context():
-    db.create_all()
+# database already created
+# with app.app_context():
+    # db.create_all()
 
 #create a route decorator
 @app.route('/')
@@ -40,17 +44,6 @@ def index():
     favorite_pizza=favorite_pizza
     )
 
-
-# class SignupForm(Form):
-#     age = IntegerField('Age')
-
-# def validate_age(form, field):
-#     if field.data < 13:
-#         raise ValidationError("We're sorry, you must be 13 or older to register")
-
-# class MyForm(Form):
-#     first_name = StringField('First Name', validators=[validators.input_required()])
-#     last_name  = StringField('Last Name', validators=[validators.optional()])
 
 #create form class
 class NamerForm(FlaskForm):
@@ -107,32 +100,3 @@ def name():
         form.name.data = ''
         flash("Form Submitted Successfully")
     return render_template("name.html", name=name, form=form)
-
-
-# def register(request):
-#     form = RegistrationForm(request.POST)
-#     if request.method == 'POST' and form.validate():
-#         user = User()
-#         user.username = form.username.data
-#         user.email = form.email.data
-#         user.save()
-#         redirect('register')
-#     return render_response('register.html', form=form)
-
-# app.route('/name', methods=['GET', 'POST'])
-# def name():
-#     name = None
-#     form = NamerForm()
-#     #validate form
-#     if form.validate_on_submit():
-#         name=form.name.data
-#         form.name.data = ''
-#     return render_template('name.html',
-#     name = name,
-#     form = form)
-
-# def create_app():
-#     app = Flask(__name__)
-#     with app.app_context():
-#         db.create_all()
-#     return app
